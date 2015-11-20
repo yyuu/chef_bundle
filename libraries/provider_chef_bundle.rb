897e20cf08832a0259e2848ee7e2b593c3ef7256
Yamashita, Yuu <peek824545201@gmail.com>
2015-11-20 08:23:56 +0000

    fix `NoMethodError` on Chef 11.x

--- libraries/provider_chef_bundle.rb
+++ libraries/provider_chef_bundle.rb
@@ -3,7 +3,9 @@ require "chef/resource/execute"
 class Chef
   class Provider
     class ChefBundle < Chef::Provider::Execute
-      provides :chef_bundle
+      if respond_to?(:provides) # chef < 12
+        provides :chef_bundle
+      end
 
       alias_method :action_install, :action_run
     end
