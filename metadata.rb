87b44ea3025f1e8c66f16c84a30014e67c197e56
Yamashita, Yuu <peek824545201@gmail.com>
2015-11-20 06:00:54 +0000

    create new project

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef_bundle'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef_bundle'
+long_description 'Installs/Configures chef_bundle'
+version          '0.1.0'
